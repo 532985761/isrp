68d9640467656a5b4d639aa871935f19f6ba6dc5
张洪志 <532985761@qq.com>
2022-06-20 19:08:52 +0800

    模块初始化

--- isrp_order/src/main/java/com/grouptwo/isrp/controller/TestController.java
+++ isrp_order/src/main/java/com/grouptwo/isrp/controller/TestController.java
@@ -1,17 +1,24 @@
 package com.grouptwo.isrp.controller;
 
+import com.grouptwo.isrp.entity.IsrpGoodsCategoryFirst;
+import com.grouptwo.isrp.service.IsrpGoodsCategoryFirstService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 
 @RestController
 @RequestMapping("/t")
 public class TestController {
 
-
+    @Resource
+    IsrpGoodsCategoryFirstService isrpGoodsCategoryFirstService;
     @GetMapping("/test")
-    public String test(){
-        return "ok";
+    public ResponseEntity<IsrpGoodsCategoryFirst> test(){
+
+        return ResponseEntity.ok(this.isrpGoodsCategoryFirstService.queryById(1));
     }
 }
--- isrp_user/src/main/java/com/grouptwo/isrp/controller/TestController.java
+++ isrp_user/src/main/java/com/grouptwo/isrp/controller/TestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
+
 
 @RestController
 @RequestMapping("/t")

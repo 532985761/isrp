2c8c55819e06bfd1cc70354e0707391b53bab3e0
张洪志 <532985761@qq.com>
2022-06-21 09:39:38 +0800

    路由配置完成

---
+++ isrp_gateway/src/main/java/com/grouptwo/isrp/IsrpGatewayApplication.java
@@ -0,0 +1,34 @@
+package com.grouptwo.isrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author 张洪志
+ */
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
+public class IsrpGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsrpGatewayApplication.class, args);
+    }
+    public static void disableWarning() {
+        try {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            Unsafe u = (Unsafe) theUnsafe.get(null);
+
+            Class cls = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field logger = cls.getDeclaredField("logger");
+            u.putObjectVolatile(cls, u.staticFieldOffset(logger), null);
+        } catch (Exception e) {
+            // ignore
+        }
+    }
+
+
+}

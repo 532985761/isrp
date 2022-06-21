5178a920ab550e1279adb7fd08738a01ca116889
张洪志 <532985761@qq.com>
2022-06-21 10:38:49 +0800

    公共模块配置

--- isrp_common/src/main/java/com/grouptwo/isrp/common/exception/GlobalException.java
+++ isrp_common/src/main/java/com/grouptwo/isrp/common/exception/GlobalException.java
@@ -33,66 +33,66 @@ public class GlobalException {
         LOGGER.error("找不到资源："+exception.getMessage());
         return new ResponseEntity("资源不存在",HttpStatus.NOT_FOUND);
     }
-//    /**
-//     * 非法请求异常
-//     * @param exception
-//     * @return
-//     */
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(value = IllegalArgumentException.class)
-//    public res badRequestException(IllegalArgumentException exception) {
-//        LOGGER.error("非法请求："+exception.getMessage(),exception);
-//        return ResBean.fail("非法请求，请求失败！");
-//    }
-//
-//
-//
-//    /**
-//     * 请求方式错误
-//     * @param exception
-//     * @return
-//     */
-//    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-//    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-//    public ResBean errorHandler(HttpRequestMethodNotSupportedException exception) {
-//        LOGGER.error("请求方法异常："+exception.getMessage());
-//        return ResBean.fail(405L, "请求方式错误");
-//    }
-//
-//    /**
-//     * 请求参数错误
-//     * @param exception
-//     * @return
-//     */
-//    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-//    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-//    public ResBean errorHandler(HttpMediaTypeNotSupportedException exception) {
-//        LOGGER.error("请求参数异常："+exception.getMessage(),exception);
-//        return ResBean.fail(415L, "请求参数错误");
-//    }
-//
-//    /**
-//     * 数据库异常
-//     * @param exception
-//     * @return
-//     */
-//    @ExceptionHandler(SQLException.class)
-//    public ResBean mySQLException(SQLException exception) {
-//        LOGGER.error("数据库异常："+exception.getMessage(),exception);
-//        return ResBean.fail("数据库异常，操作失败！");
-//    }
-//
-//    /**
-//     * 服务器异常
-//     * @param exception
-//     * @returnz
-//     */
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    @ExceptionHandler(value = Exception.class)
-//    public ResBean errorHandler(Exception exception) {
-//        LOGGER.error("服务器异常："+exception.getMessage(),exception);
-//        return ResBean.fail("服务器异常");
-//    }
+    /**
+     * 非法请求异常
+     * @param exception
+     * @return
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity badRequestException(IllegalArgumentException exception) {
+        LOGGER.error("非法请求："+exception.getMessage(),exception);
+        return new ResponseEntity("非法请求，请求失败！",HttpStatus.BAD_REQUEST);
+    }
+
+
+
+    /**
+     * 请求方式错误
+     * @param exception
+     * @return
+     */
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity errorHandler(HttpRequestMethodNotSupportedException exception) {
+        LOGGER.error("请求方法异常："+exception.getMessage());
+        return new ResponseEntity("请求方式错误",HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    /**
+     * 请求参数错误
+     * @param exception
+     * @return
+     */
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity errorHandler(HttpMediaTypeNotSupportedException exception) {
+        LOGGER.error("请求参数异常："+exception.getMessage(),exception);
+        return new ResponseEntity("请求参数错误",HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+    }
+
+    /**
+     * 数据库异常
+     * @param exception
+     * @return
+     */
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity mySQLException(SQLException exception) {
+        LOGGER.error("数据库异常："+exception.getMessage(),exception);
+        return new ResponseEntity("数据库异常，操作失败！",HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * 服务器异常
+     * @param exception
+     * @returnz
+     */
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity errorHandler(Exception exception) {
+        LOGGER.error("服务器异常："+exception.getMessage(),exception);
+        return new ResponseEntity("服务器异常",HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 
 }

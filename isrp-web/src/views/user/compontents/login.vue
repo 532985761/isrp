7e2f26376925579240b39e55b7cebcf31c8e41dc
张洪志 <532985761@qq.com>
2022-06-23 10:57:03 +0800

    测试

---
+++ isrp-web/src/views/user/compontents/login.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="bg-dark-800 max-h-10"></el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>

d635a877204fe2928faf5b016df5beafb4bdcd10
Wilburn <1184246778@qq.com>
2022-06-24 21:33:57 +0800

    更新全局icon

--- isrp-web/src/main.ts
+++ isrp-web/src/main.ts
@@ -5,12 +5,17 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 
 const app = createApp(App)
 
 // Windi CSS
 import 'virtual:windi.css'
+// ElementPlusIcons
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 // Pinia
 const store = createPinia()
 const cookie = useCookies()
